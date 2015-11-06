6482a346a15d7da3cad75723cebd95bac9e57b30
Victor Arribas <v.arribas.urjc@gmail.com>
2015-11-06 22:53:24 +0100

    opencv: fix non included libopencv-dev

--- make_opencv2.4.sh
+++ make_opencv2.4.sh
@@ -5,7 +5,7 @@
 # License: GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
 
 ./make_packages_for.sh \
-"libopencv-.*2.4|libopencv-.*-dev|libcv-dev|libcv2.4|python-opencv" \
+"libopencv-.*2.4|libopencv.*-dev|libcv-dev|libcv2.4|python-opencv" \
 2.4.11 \
 all \
 "OpenCV fake-deb"
